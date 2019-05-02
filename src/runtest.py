3b3fd36d9f8ffdad2d14a6420de24a06e6221034
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-05-02 11:57:21 +0000

    update files

--- src/runtest.py
+++ src/runtest.py
@@ -1,7 +1,7 @@
 import os
 
-not_moving_videos = "no_moving.txt"
-moving_videos = "moving.txt"
+not_moving_videos = "not_moving_filter.txt"
+moving_videos = "moving_filter.txt"
 error = "error.txt"
 def find_good_data(cc=0):
     c = 0
@@ -32,4 +32,52 @@ def find_good_data(cc=0):
                         f2.write(name+"\n") 
             print(c)
             c += 1
-find_good_data(0)
+
+def run_code(mode,threshold,filename):
+    c = 0
+    with open(filename) as f:
+        for l in f:
+            if c>threshold:
+                break
+            l = l.strip()
+            name = l
+            os.chdir("../PyTorch-YOLOv3")
+            try:
+                os.system("python3 detect.py --root_dir=../../{0} --mode={1} --img_size=608 --output_dir=output_{1}".format(l,mode))
+                os.chdir("../src")
+            except:
+                os.chdir("../src")
+                with open("errorx.txt","a") as f2:
+                        f2.write(name+"\n") 
+            print(c)
+            c += 1
+
+def find_lower_frame():
+    rs = {}
+    with open(not_moving_videos) as f:
+        for l in f:
+            l = l.strip()
+            ll = os.listdir("../../{0}".format(l))
+            rs[l] = len(ll)
+    rs = sorted(rs.items(),key=lambda x:x[1])
+    with open(not_moving_videos,'w') as f:
+        for l in rs:
+            if l[1]<230:
+                f.write(l[0]+"\n")
+    rs = {}
+    with open(moving_videos) as f:
+        for l in f:
+            l = l.strip()
+            ll = os.listdir("../../{0}".format(l))
+            rs[l] = len(ll)
+    rs = sorted(rs.items(),key=lambda x:x[1])
+    with open(moving_videos,'w') as f:
+        for l in rs:
+            if l[1]<230:
+                f.write(l[0]+"\n")
+
+def compare_acc_time(mode1,mode2,filename,threshold):
+    pass
+
+find_lower_frame()
+#run_code("origin",20,not_moving_videos)
