1d2540c2b77c348a6aacd21e409a941dc43beb54
tianchen <tczhong24@gmail.com>
2019-05-02 10:10:48 -0400

    update test file

--- src/runtest.py
+++ src/runtest.py
@@ -43,9 +43,13 @@ def run_code(mode,threshold,filename):
             name = l
             os.chdir("../PyTorch-YOLOv3")
             try:
-                os.system("python3 detect.py --root_dir=../../{0} --mode={1} --img_size=608 --output_dir=output_{1}".format(l,mode))
+                if mode=="origin":
+                    os.system("python3 detect.py --root_dir=../../{0} --mode={1} --img_size=608 --output_dir=output_{1}/".format(l,mode))
+                else:
+                    os.system("python3 detect.py --root_dir=../../{0} --mode={1} --output_dir=output_{1}/".format(l,mode))
                 os.chdir("../src")
-            except:
+            except Exception as e:
+                print(e)
                 os.chdir("../src")
                 with open("errorx.txt","a") as f2:
                         f2.write(name+"\n") 
@@ -62,7 +66,7 @@ def find_lower_frame():
     rs = sorted(rs.items(),key=lambda x:x[1])
     with open(not_moving_videos,'w') as f:
         for l in rs:
-            if l[1]<230:
+            if l[1]>50:
                 f.write(l[0]+"\n")
     rs = {}
     with open(moving_videos) as f:
@@ -73,11 +77,43 @@ def find_lower_frame():
     rs = sorted(rs.items(),key=lambda x:x[1])
     with open(moving_videos,'w') as f:
         for l in rs:
-            if l[1]<230:
+            if l[1]>50:
                 f.write(l[0]+"\n")
 
+def read_infere_time(mode,filename):
+    total_time = 0.0
+    with open("../PyTorch-YOLOv3/output_{}/output/{}/infer_time.txt".format(mode,filename)) as f:
+        for line in f:
+            line = line.strip()   
+            if line[0] == "T":
+                total_time = (line.split()[2])
+            #if line[0] == "A":
+            #    self.averge_time = line.split()[2]
+    return total_time
+def read_acc_time(mode,filename):
+    acc = 0.0
+    with open("../PyTorch-YOLOv3/output_{}/output/{}/acc.txt".format(mode,filename)) as f:
+        acc = float(f.readline())
+    return acc
 def compare_acc_time(mode1,mode2,filename,threshold):
-    pass
-
-find_lower_frame()
-#run_code("origin",20,not_moving_videos)
+    total_time1 = 0.0
+    total_time2 = 0.0
+    acc1 = 0
+    acc2 = 0
+    
+    total_time1 = read_infere_time(mode1,filename[21:])
+    total_time2 = read_infere_time(mode2,filename[21:])
+    acc1 = read_acc_time(mode1,filename[21:])
+    acc2 = read_acc_time(mode2,filename[21:])
+    print(" total time 1 is {}".format(total_time1))
+    print(" total time 2 is {}".format(total_time2))
+    print(" acc 1 is {}".format(acc1))
+    print(" acc 2 is {}".format(acc2))
+        
+    
+#find_lower_frame()
+run_code("origin",30,not_moving_videos)
+run_code("cropped",30,not_moving_videos)
+run_code("psnr",30,not_moving_videos)
+run_code("net",30,not_moving_videos)
+#compare_acc_time("origin","cropped","image/Data/VID/train/ILSVRC2015_VID_train_0000/ILSVRC2015_train_00051019",0)
